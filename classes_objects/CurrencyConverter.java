2fbb9da11a3c20592ac8a7494e5597e9419827c5
vennela28 <vennela.jobs@gmail.com>
2019-10-23 04:37:15 +0530

    Currency Converter

--- classes_objects/CurrencyConverter.java
+++ classes_objects/CurrencyConverter.java
@@ -1,21 +1,30 @@
 import java.lang.*;
 public class CurrencyConverter{
-        static int rupee = 63;
-        static int dirham = 3;
-        static int _yen = 107;
-        static int $australian = 2;
-        static int dollar;
-        static final int Rupee = 63;
-	public static void printCurrencies(){
-        System.out.println("yen "+ _yen);
+         int rupee = 63;
+         int dirham = 3;
+		 int real = 3;   // brazilian
+ 
+         int chilean_peso = 595;
+		 int mexican_peso = 18;   
+ 
+         int _yen = 107;
+         int $australian = 2;
+         int dollar;
+         int Rupee = 63;
+	void printCurrencies(){
+
         System.out.println("australian "+ $australian);
         System.out.println("rupee "+ rupee);
         System.out.println("dirham "+ dirham);
+		System.out.println("real: " + real);
+        System.out.println("chilean_peso: " + chilean_peso);
+        System.out.println("mexican_peso: " + mexican_peso);
+		System.out.println("yen "+ _yen);
         System.out.println("dollar "+ dollar);
         System.out.println("Rupee "+ Rupee);
 	}
 	public static void main(String[] args){
-   //     CurrencyConverter cc = new CurrencyConverter();
-        printCurrencies();
+        CurrencyConverter cc = new CurrencyConverter();
+        cc.printCurrencies();
 	}
 }
