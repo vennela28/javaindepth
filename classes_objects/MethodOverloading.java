c416b2ad2f7806d29f88dc0ed0742e22173b1cb9
vennela28 <vennela.jobs@gmail.com>
2019-10-28 00:02:37 +0530

    Method Overloading

--- classes_objects/MethodOverloading.java
+++ classes_objects/MethodOverloading.java
@@ -1,8 +1,8 @@
 /*
 Method Overloading is having different methods with the same name but different parameter lists (number and types of method parameters).
-MUST change the parameter list
-changing the return type doesn't matter
-both instance and static methods can be overloaded
+- MUST change the parameter list
+- changing the return type doesn't matter
+- both instance and static methods can be overloaded
 
 // VALID Method Overloading examples
 int updateProfile(int newId) {}  #1
@@ -10,15 +10,17 @@ int updateProfile(int newId, char gender) {}  #2
 int updateProfile(char gender, int newId) {}  #3
 int updateProfile(short newId) {}  #4
 byte b = 50;
-updateProfile(b);  the compiler picks up #4 as it is more specific than 1 i.e. it is next larger datatype after byte
+updateProfile(b);  the compiler picks up #4 as it is more specific than 1 because it is next larger datatype after byte
 The compiler picks a method that has most specific compatible datatype.
 During compilation process, compiler writes into the bytecode about the method that needs to be invoked at runtime by JVM. 
 This process of the compiler picking up the method to be invoked and subsequently JVM using that information at runtime is called method binding.
 
 // INVALID Method Overloading examples  - get a compiler error saying that a duplicate method is added.
-void updateProfile(int newId) {} - instance method
+			
+			void updateProfile(int newId) {} - instance method
+
 void updateProfile(int id) {} - changing the parameter name doesn't work
-boolean updateProfile(int newId) {} - compiler will complain that we have a duplicate method. We get a compilation error
+boolean updateProfile(int newId) {} - compiler will complain that we have a duplicate method. We get compilation error since only the return type is different.
 static void updateProfile(int newId) {} - adding static to the method signature doesn't work
 
 PrintStream class has overloaded methods for println(), when we invoke System.out.println().  
@@ -49,6 +51,6 @@ public class MethodOverloading{
 		go(1000);    // #2 is invoked because 1000 is an int literal and the compiler would pick #2
 		
 		byte b = 10;
-		go(b);    // #3 is invoked 10 is an byte literal and the compiler would pick #3 which is the most specific next compatible datatype.
+		go(b);    // #3 is invoked since 10 is an byte literal and the compiler would pick #3 which is the most specific next compatible datatype.
 	}
 }
