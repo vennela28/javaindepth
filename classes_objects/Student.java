347a0bbe149d6491f73568f3b5371aeb7a015d0c
vennela28 <vennela.jobs@gmail.com>
2019-10-30 04:33:50 +0530

    Reinitializing object references

--- classes_objects/Student.java
+++ classes_objects/Student.java
@@ -14,7 +14,11 @@ public class Student{
 		this.age = age;
 		this.grades = grades;
 	}
-	
+/*	
+	Student(){
+		
+	}
+*/
 	//2.
 	Student(String name){
 		this.name = name;
@@ -38,21 +42,42 @@ public class Student{
 	public static void main(String[] args){
 		int[] ages = new int[]{27, 28, 29};
 		
-		Student s1 = new Student(ages[0], "Amma");
+		Student s1 = new Student(ages[0], "Ama");
 		s1.gender = "female";
 		
 		Student s2 = new Student(ages[1], "Babu");
 		s2.gender = "male";
 		
-		Student s3 = new Student(ages[2], "Venela", new char[] {'A', 'A'});
+		Student s3 = new Student(ages[2], "Vennela", new char[] {'A', 'A'});
 		s3.gender ="female";
 		
 		System.out.println("Name s1: "+s1.name);
 		System.out.println("Name s2: "+s2.name);
 		System.out.println("Name s3: "+s3.name);
 				
-		s3.updateProfile("Vennela");
+		s1.updateProfile("Amma");
 		
-		System.out.println("Name s3 corrected: "+s3.name);		
+		System.out.println("Updated Name s1: "+s1.name);	
+		
+		// Reassigning with an object reference
+		Student s4 = s1; // The memory address of s4 points to the memory address of s1. s4 and s1 are referencing the same object
+		System.out.println("Name s4: "+s4.name);
+
+		s4.updateProfile("mike");
+		System.out.println("Name s1: "+s1.name);
+		
+		s4 = s2;
+		System.out.println("Name s4: "+s4.name);
+		
+		s2 = s1;
+		System.out.println("Name s4: "+s4.name);
+		System.out.println("Name s2: "+s2.name);
+		
+		// A new object is created
+		s4 = new Student("susan"); // If a non parameterized constructor is initialized in the code, s4.name = null 
+		System.out.println("Name s4: "+s4.name); // susan is printed since a new object is created all together
+		
+		System.out.println("Name s2: "+s2.name);
+		System.out.println("Name s1: "+s1.name);
 	}
 }
